425d6e803972568fb95812c3da6a5aaf88e69503
Maksim Masterenko <mmasterenko@gmail.com>
2015-12-07 10:57:55 +0300

    first working page

--- tengProject/urls.py
+++ tengProject/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'tengProject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'tengApp.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
