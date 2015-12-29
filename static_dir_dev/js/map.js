c6e0c2300ca853ccaa74550d560bd405beed2c35
Maksim Masterenko <mmasterenko@gmail.com>
2015-12-29 14:48:37 +0300

    google map: cluster feature added

--- static_dir_dev/js/map.js
+++ static_dir_dev/js/map.js
@@ -23,6 +23,14 @@ var styles = [
     }
 ];
 
+var markers_inode = [
+    {lat: 59.950366, lng: 30.076602, title: 'Объект 1', id: 'id 1'},
+    {lat: 59.851566, lng: 30.176802, title: 'Объект 2', id: 'id 2'},
+    {lat: 60.000001, lng: 30.376602, title: 'Объект 3', id: 'id 3'},
+    {lat: 60.001000, lng: 30.576602, title: 'Объект 4', id: 'id 4'},
+    {lat: 60.010000, lng: 30.976602, title: 'Объект 5', id: 'id 5'}
+];
+
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('google-map'), {
@@ -31,4 +39,24 @@ function initMap() {
         styles: styles,
         scrollwheel: false
     });
+
+    var i, markers = [];
+
+    for (i in markers_inode) {
+        var latLng = new google.maps.LatLng(markers_inode[i].lat, markers_inode[i].lng);
+        var marker = new google.maps.Marker({
+            'position': latLng,
+            'title': markers_inode[i].title,
+            //'label': 'метка',
+            'teng_id': markers_inode[i].id
+        });
+        markers.push(marker);
+        marker.addListener('click', function() {
+            //map.setCenter(this.getPosition());
+            console.log(this.teng_id)
+            alert(this.teng_id)
+        });
+    }
+    var markerCluster = new MarkerClusterer(map, markers);
+
 }
