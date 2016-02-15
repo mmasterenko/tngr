d31e02f07fdb6b17d2bd899465f0cab9b4a36c61
Maksim Masterenko <mmasterenko@gmail.com>
2016-02-15 13:49:59 +0300

    fix

--- tengApp/templatetags/google_map.py
+++ tengApp/templatetags/google_map.py
@@ -5,7 +5,7 @@ import json
 register = template.Library()
 
 
-@register.simple_tag()
+@register.simple_tag
 def get_markers():
     markers = list(Project.objects.values('latitude', 'longitude', 'name', 'company', 'desc', 'image'))
     return json.dumps(markers)
